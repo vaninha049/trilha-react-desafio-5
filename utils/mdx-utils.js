8199c869dcf82e69500be5ede55666a79c4cec81
Vanessa Fontes <vaninha049@hotmail.com>
2024-11-16 19:55:30 -0300

    Atualizando o post

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -14,6 +14,18 @@ export const getPostBySlug = async (id) => {
 
     //TODO: BUSCAR UM POST EM ESPECIFICO.
     //const {data} = await api.get(`/post?id=eq.${id}`)
+    try {
+        // Faz uma requisição para buscar o post específico pelo ID
+        const { data } = await api.get(`/posts?id=eq.${id}`);
+        
+        // Verifica se há dados retornados
+        if (data && data.length > 0) {
+            return data[0]; // Retorna o primeiro item (caso a resposta seja uma lista)
+        }
 
-    return {}
+        return {};
+    } catch (error) {
+        console.error("Erro ao buscar o post:", error);
+        return {}; // Retorna um objeto vazio em caso de erro
+    }
 }
